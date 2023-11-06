71d9040626f1233ff5ffe83c556f9bf85ebe3253
CE11-Kishan <om.kishan2001@gmail.com>
2023-11-06 18:22:03 +0530

    End of section16

---
+++ client/src/app/modals/roles-modal/roles-modal.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+import { BsModalRef } from 'ngx-bootstrap/modal';
+
+@Component({
+      selector: 'app-roles-modal',
+      templateUrl: './roles-modal.component.html',
+      styleUrls: ['./roles-modal.component.css']
+})
+export class RolesModalComponent implements OnInit {
+      username = '';
+      availableRoles: any[] = [];
+      selectedRoles: any[] = [];
+
+      constructor(public bsModalRef: BsModalRef) { }
+
+      ngOnInit(): void {
+      }
+
+      updateChecked(checkedValues: string){
+            const index = this.selectedRoles.indexOf(checkedValues);
+            index != -1 ? this.selectedRoles.splice(index, 1) : this.selectedRoles.push(checkedValues);
+      }
+
+}
