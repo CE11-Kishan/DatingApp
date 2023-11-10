2f2d27e1155f79816f09da77121f7deeea220832
CE11-Kishan <om.kishan2001@gmail.com>
2023-11-10 17:00:36 +0530

    bug fix

--- API/Program.cs
+++ API/Program.cs
@@ -27,12 +27,11 @@ else
       connUrl = connUrl.Replace("postgres://", string.Empty);
       var pgUserPass = connUrl.Split("@")[0];
       var pgHostPortDb = connUrl.Split("@")[1];
-      var pgHostPort = pgHostPortDb.Split("/")[0];
+      var pgHost = pgHostPortDb.Split("/")[0];
       var pgDb = pgHostPortDb.Split("/")[1];
       var pgUser = pgUserPass.Split(":")[0];
       var pgPass = pgUserPass.Split(":")[1];
-      var pgHost = pgHostPort.Split(":")[0];
-      var pgPort = pgHostPort.Split(":")[1];
+      var pgPort = "5432";
 
       connString = $"Server={pgHost};Port={pgPort};User Id={pgUser};Password={pgPass};Database={pgDb};";
 
